b6c0473114a3d3b40ea2353159e94ec653e5fffb
Nizar Zariouh <nizarzariouh05@gmail.com>
2024-02-01 17:10:40 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace ArduinoWF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.connectbutton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.upbutton = new System.Windows.Forms.Button();
+            this.downbutton = new System.Windows.Forms.Button();
+            this.rightbutton = new System.Windows.Forms.Button();
+            this.leftbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(27, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(208, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // connectbutton
+            // 
+            this.connectbutton.Location = new System.Drawing.Point(226, 62);
+            this.connectbutton.Name = "connectbutton";
+            this.connectbutton.Size = new System.Drawing.Size(75, 23);
+            this.connectbutton.TabIndex = 3;
+            this.connectbutton.Text = "Connect";
+            this.connectbutton.UseVisualStyleBackColor = true;
+            this.connectbutton.Click += new System.EventHandler(this.connectbutton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(35, 39);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Automated";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // upbutton
+            // 
+            this.upbutton.Location = new System.Drawing.Point(77, 80);
+            this.upbutton.Name = "upbutton";
+            this.upbutton.Size = new System.Drawing.Size(35, 23);
+            this.upbutton.TabIndex = 5;
+            this.upbutton.Text = "U";
+            this.upbutton.UseVisualStyleBackColor = true;
+            this.upbutton.Click += new System.EventHandler(this.upbutton_Click);
+            // 
+            // downbutton
+            // 
+            this.downbutton.Location = new System.Drawing.Point(77, 109);
+            this.downbutton.Name = "downbutton";
+            this.downbutton.Size = new System.Drawing.Size(35, 23);
+            this.downbutton.TabIndex = 6;
+            this.downbutton.Text = "D";
+            this.downbutton.UseVisualStyleBackColor = true;
+            this.downbutton.Click += new System.EventHandler(this.downbutton_Click);
+            // 
+            // rightbutton
+            // 
+            this.rightbutton.Location = new System.Drawing.Point(119, 109);
+            this.rightbutton.Name = "rightbutton";
+            this.rightbutton.Size = new System.Drawing.Size(35, 23);
+            this.rightbutton.TabIndex = 7;
+            this.rightbutton.Text = "R";
+            this.rightbutton.UseVisualStyleBackColor = true;
+            this.rightbutton.Click += new System.EventHandler(this.rightbutton_Click);
+            // 
+            // leftbutton
+            // 
+            this.leftbutton.Location = new System.Drawing.Point(36, 109);
+            this.leftbutton.Name = "leftbutton";
+            this.leftbutton.Size = new System.Drawing.Size(35, 23);
+            this.leftbutton.TabIndex = 8;
+            this.leftbutton.Text = "L";
+            this.leftbutton.UseVisualStyleBackColor = true;
+            this.leftbutton.Click += new System.EventHandler(this.leftbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "LCD Display Text";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Connect with a port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Automatic Servo Movement";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Servo Movement Control";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 293);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.leftbutton);
+            this.Controls.Add(this.rightbutton);
+            this.Controls.Add(this.downbutton);
+            this.Controls.Add(this.upbutton);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.connectbutton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button connectbutton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button upbutton;
+        private System.Windows.Forms.Button downbutton;
+        private System.Windows.Forms.Button rightbutton;
+        private System.Windows.Forms.Button leftbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoWF
+{
+    public partial class Form1 : Form
+    {
+
+        bool isConnected = false;
+        bool isAutomated = false;
+        String[] ports;
+        SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+            disableControls();
+            getAvailablePorts();
+            
+            foreach(var port in ports)
+            {
+                comboBox1.Items.Add(port.ToString());
+                if (ports[0] != null)
+                {
+                    comboBox1.SelectedItem = "COM4";
+                }
+
+            }
+        
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void connectbutton_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                connectToArduino();
+            }
+            else
+            {
+                disconnectFromArduino();
+            }
+        }
+
+        void getAvailablePorts()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+
+        private void connectToArduino()
+        {
+            try
+            {
+                isConnected = true;
+                string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
+                port = new SerialPort(selectedPort, 9600, Parity.None, 8, StopBits.One);
+                port.Open();
+                //port.Write("#STAR\n");
+                connectbutton.Text = "Disconnect";
+                Console.WriteLine("Connected to: " + port.PortName);
+                enableControls();
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                
+            }
+
+        }
+
+        private void disconnectFromArduino()
+        {
+            try
+            {
+                // port.Write("#STOP\n");
+                port.Write("2");
+                port.Write("");
+                checkBox1.Checked = false;
+                connectbutton.Text = "Connect";
+                isConnected = false;
+                disableControls();
+                port.Close();
+            } catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+               
+        }
+        
+        // A way to send text to display in a 16x2 I2C LCD (currently unused)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                string sentText = "#TEXT" + textBox1.Text + "\n";
+                port.Write(sentText);
+                Console.WriteLine("Message sent: " + sentText);
+            }
+        }
+        private void enableControls()
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            checkBox1.Enabled = true;
+        }
+
+        private void disableControls()
+        {
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            checkBox1.Enabled = false;
+            textBox1.Text = "";
+        }
+        // automated servo movement checkbox
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                if (checkBox1.Checked)
+                {
+                    string sentText = "1";
+                    Console.WriteLine("Automated servo activated" + " " + sentText);
+                    isAutomated = true;
+                    disableServoControl();
+                    port.Write(sentText);
+                }
+                else if (!checkBox1.Checked)
+                {
+                    string sentText = "2";
+                    Console.WriteLine("Automated servo deactivated" + " " + sentText);
+                    isAutomated = false;
+                    enableServoControl();
+                    port.Write(sentText);
+                }
+               
+            }
+        }
+
+        private void disableServoControl()
+        {
+            upbutton.Enabled = false;
+            downbutton.Enabled = false;
+            rightbutton.Enabled = false;
+            leftbutton.Enabled = false;
+
+        }
+
+        private void enableServoControl() {
+            upbutton.Enabled = true;
+            downbutton.Enabled = true;
+            rightbutton.Enabled = true;
+            leftbutton.Enabled = true;
+        }
+
+        // control
+        private void upbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sentText = "U";
+                Console.WriteLine("Sent up instruction: " + sentText);
+                port.Write(sentText);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            }
+
+        private void downbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sentText = "D";
+                Console.WriteLine("Sent down instruction: " + sentText);
+                port.Write(sentText);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void rightbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sentText = "R";
+                Console.WriteLine("Sent right instruction: " + sentText);
+                port.Write(sentText);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void leftbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sentText = "L";
+                Console.WriteLine("Sent left instruction: " + sentText);
+                port.Write(sentText);
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
